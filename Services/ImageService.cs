2f4866548c378941b49d6ed06fff44b80610bda0
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2025-08-16 02:20:37 +0700

    Implement image services.

---
+++ Services/ImageService.cs
@@ -0,0 +1,134 @@
+using Grpc.Core;
+using Backend;
+using Backend.FileSystem;
+using PathLib;
+using Google.Protobuf;
+
+namespace Backend.Services;
+
+public class ImageService(ILogger<GreeterService> logger) : Image.ImageBase
+{
+    private readonly ILogger<GreeterService> _logger = logger;
+
+    const string MIME_TYPE = "images/webp";
+    const int STREAM_SIZE = 10 * 1024;
+
+    public override async Task Thumbnail(ThumbnailRequest request,
+        IServerStreamWriter<ImageStreamResponse> responseStream,
+        ServerCallContext context)
+    {
+        var data = CreateListThumbnail(request.Path);
+        var filename = new PosixPath(request.Path).Filename;
+
+        for (int i = 0; i < data.Length; i += STREAM_SIZE)
+        {
+            var count = Math.Max(STREAM_SIZE, data.Length - i);
+
+            await responseStream.WriteAsync(new ImageStreamResponse
+            {
+                Filename = string.Format("{}.webp", filename),
+                ContentType = MIME_TYPE,
+                Size = count,
+                Data = ByteString.CopyFrom(data, i, count)
+            });
+        }
+
+        return;
+    }
+
+    public override async Task View(ViewRequest request, IServerStreamWriter<ImageStreamResponse> responseStream, ServerCallContext context)
+    {
+        var pathObj = new PosixPath(request.Path);
+        var filename = pathObj.Filename;
+
+        if (MimeTypes.GetMimeType(filename) == "image/gif")
+        {
+            using var stream = GetStream(pathObj);
+            using var ms = new MemoryStream();
+            stream.CopyTo(ms);
+            var data = ms.ToArray();
+
+            await WriteStream(responseStream, data, filename, "image/gif");
+            return;
+        }
+
+        else
+        {
+            using var stream = GetStream(pathObj);
+            using var image = NetVips.Image.NewFromStream(stream);
+
+            byte[] output;
+            if (image.Width < Configurations.ViewImageWidth || image.Height < Configurations.ViewImageHeight)
+            {
+                output = image.WebpsaveBuffer();
+            }
+            else
+            {
+                using var thumb =
+                    image.ThumbnailImage(width: Configurations.ViewImageWidth, height: Configurations.ViewImageHeight,
+                                         crop: NetVips.Enums.Interesting.None);
+
+                output = thumb.WebpsaveBuffer();
+            }
+
+            await WriteStream(responseStream, output, $"{filename}.webp", MIME_TYPE);
+
+            return;
+        }
+    }
+
+    public byte[] CreateListThumbnail(string path, int width = Configurations.ListThumbnailWidth, int height = Configurations.ListThumbnailHeight)
+    {
+        using var stream = GetStream(new PosixPath(path));
+        using var image = NetVips.Image.NewFromStream(stream);
+
+        var scale =
+            image.Width > image.Height ? (double)width / (double)image.Width : (double)height / (double)image.Height;
+
+        using var thumb = image.Resize(scale);
+
+        return thumb.WebpsaveBuffer();
+    }
+
+    public byte[] CreateGridThumbnail(string path)
+    {
+        using var stream = GetStream(new PosixPath(path));
+        using var image = NetVips.Image.NewFromStream(stream);
+
+        using var thumb =
+            image.ThumbnailImage(width: Configurations.GridThumbnailWidth, height: Configurations.GridThumbnailHeight,
+                                 crop: NetVips.Enums.Interesting.Entropy);
+
+        return thumb.WebpsaveBuffer();
+    }
+
+    private static Stream GetStream(PosixPath path)
+    {
+        var (physicalPath, archivePath, hasArchivePath) = PathUtility.SplitPathAfterArchiveFile(path);
+
+        if (hasArchivePath)
+        {
+            return ArchiveFS.ReadFile(physicalPath, archivePath);
+        }
+        else
+        {
+            return PhysicalFS.ReadFile(physicalPath);
+        }
+    }
+
+    private static async Task WriteStream(IServerStreamWriter<ImageStreamResponse> responseStream, byte[] data, string filename, string mimeType)
+    {
+        for (int i = 0; i < data.Length; i += STREAM_SIZE)
+        {
+            var count = Math.Max(STREAM_SIZE, data.Length - i);
+
+            await responseStream.WriteAsync(new ImageStreamResponse
+            {
+                Filename = filename,
+                ContentType = mimeType,
+                Size = count,
+                Data = ByteString.CopyFrom(data, i, count)
+            });
+        }
+    }
+}
